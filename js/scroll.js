08ce85b7530a71fc04f7e373b2576ea51eff9d3b
Kaio Lopes <kaio.loopes18@gmail.com>
2022-09-22 10:03:22 -0300

    Ajustando responsividade

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,22 @@
 export default function scrolls(){
     const alvo = Array.from(document.querySelectorAll(".section-article article"))
-
+    let doc = document.body.clientHeight;
+    let win = window.innerHeight;
+    if(doc > win){
+        window.addEventListener("scroll", showArticle)
+    } else {
+        alvo.forEach(article => {article.style.opacity = 1})
+    }
     function showArticle(){
-        alvo.forEach(article => {
-            if(article.getBoundingClientRect().top < innerHeight / 1.8){
-                article.classList.add("show")
+            alvo.forEach(article => {
+                if(article.getBoundingClientRect().top < innerHeight / 1.8){
+                    article.classList.add("show")
+                }
+            })
+    
+            if(Array.from(document.querySelectorAll(".show")).length === alvo.length){
+                window.removeEventListener("scroll", showArticle)
             }
-        })
-
-        if(Array.from(document.querySelectorAll(".show")).length === alvo.length){
-            window.removeEventListener("scroll", showArticle)
-        }
     }
 
-    window.addEventListener("scroll", showArticle)
 }
