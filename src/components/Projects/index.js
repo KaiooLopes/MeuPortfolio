e9203dadbdfc2a624418052f1f14416206594445
Kaio Lopes <kaio.loopes18@gmail.com>
2023-04-25 09:18:10 -0300

    Add Project ultradentino!

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,7 +4,7 @@ import { ProjectsContainer, ShowDetails } from "./styles";
 /* IMAGES */
 import wordlol from "../../imgsProjetos/wordlol.png";
 import todolistonline from "../../imgsProjetos/todolistonline.png";
-import ultradentino from "../../imgsProjetos/ultradentino.png";
+import UltraDentino from "../../imgsProjetos/UltraDentino.png";
 import cinerate from "../../imgsProjetos/cinerate.png";
 import misenscene from "../../imgsProjetos/misenscene.png";
 import { useState } from "react";
@@ -61,7 +61,7 @@ const Projects = () => {
       tecs: ["React.js", "Javascript", "Css", "Styled-components", "HTML 5"],
     },
     {
-      img: ultradentino,
+      img: UltraDentino,
       title: "Ultra Dentino",
       subtitle: "Site Ficticio Ultra Dentino",
       link: "https://ultradentino.netlify.app/",
@@ -94,7 +94,7 @@ const Projects = () => {
           <img src={todolistonline} alt="ToDoList" />
         </button>
         <button onClick={() => handleProjectDetail(2)}>
-          <img src={ultradentino} alt="Ultra Dentino" />
+          <img src={UltraDentino} alt="Ultra Dentino" />
         </button>
         <button onClick={() => handleProjectDetail(3)}>
           <img src={cinerate} alt="Cinerate" />
